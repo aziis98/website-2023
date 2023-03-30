8df5b30f516b244226ef5500e42e290d292fbf38
Antonio De Lucreziis <antonio.delucreziis@gmail.com>
2023-03-30 14:26:54 +0200

    Updated output folder (I like "out/" better)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+	outDir: 'out/',
 	markdown: {
 		shikiConfig: {
 			theme: "github-light",
