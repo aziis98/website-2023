74948c0358350729f25b9fcf4646901b866d6196
Antonio De Lucreziis <antonio.delucreziis@gmail.com>
2025-06-03 23:30:50 +0200

    llm draft conversion from typst code

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,16 @@ import { defineConfig } from 'astro/config'
 import preact from '@astrojs/preact'
 
 import mdx from '@astrojs/mdx'
+
 import remarkMath from 'remark-math'
+// import rehypeKatex from 'rehype-katex'
 
 // https://astro.build/config
 export default defineConfig({
     outDir: 'out/',
     markdown: {
+        remarkPlugins: [remarkMath],
+        // rehypePlugins: [rehypeKatex],
         shikiConfig: {
             theme: 'github-light',
         },
@@ -16,6 +20,7 @@ export default defineConfig({
         preact(),
         mdx({
             remarkPlugins: [remarkMath],
+            // rehypePlugins: [rehypeKatex],
         }),
     ],
 })
